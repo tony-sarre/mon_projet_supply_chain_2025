642a2578a7830d39ceb7b0211e312134d3bce135
Dash_Call_Center <sarre.tony@gmail.com>
2025-10-28 10:55:05 +0000

    Test : Commenting layout for deploy

--- supply_chain_analytics.py
+++ supply_chain_analytics.py
@@ -3174,7 +3174,7 @@ def page_overview(master_df: pd.DataFrame = None):
             html.Div(dbc.Row([
                 dbc.Col(html.Div(f"Détails Produits - {len(available_cols)} colonnes", className="section-title"),
                         md=8),
-               # dbc.Col(html.Div(action_buttons, style={"textAlign": "right"}), md=4)
+                dbc.Col(html.Div(action_buttons, style={"textAlign": "right"}), md=4)
             ])),
             html.Br(),
             #table
@@ -5975,7 +5975,7 @@ def on_chat(n_clicks, user_text, history, uploaded_json, master_json):
 
 # ------------------------------ Run ------------------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
+    port = int(os.environ.get("PORT", 10000))
     app.run(debug=True, host="0.0.0.0", port=port)
 '''from waitress import serve
 from supply_chain_analytics import app  # Assurez-vous que 'app' est bien l'instance de votre application Dash/Flask
